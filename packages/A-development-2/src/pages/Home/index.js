c5a14f0db2759d0a86fb39b43a88b0f2bd5b698a
tdzsx789 <405094616@qq.com>
2025-11-25 20:34:50 +0800

    update:完成社区发展2

--- packages/A-development-2/src/pages/Home/index.js
+++ packages/A-development-2/src/pages/Home/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './index.css';
 import startImg from '../../assets/start.png';
+import startImg2 from '../../assets/start2.png';
+import startImg3 from '../../assets/start3.png';
 import coverImg from '../../assets/cover.jpg';
 
-function Home({ onLearnMore }) {
+function Home({ onLearnMore, onLearnMore2, onLearnMore3 }) {
   return (
     <div className="home-page" style={{ backgroundImage: `url(${coverImg})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
-      <header className="home-header">
-        <h1>城市居民委员会发展2</h1>
-        <p>A馆</p>
-      </header>
       <div className="learn-more-btn" onClick={onLearnMore}>
         <img src={startImg} alt="了解更多" />
+      </div>
+      <div className="learn-more-btn2" onClick={onLearnMore2}>
+        <img src={startImg2} alt="了解更多2" />
+      </div>
+      <div className="learn-more-btn3" onClick={onLearnMore3}>
+        <img src={startImg3} alt="了解更多3" />
       </div>
     </div>
   );
